d0fc7b0f9d86b4b0a3941aa27febabf537e66173
devopskickoff <devopskick@gmail.com>
2021-08-13 20:26:19 +0900

    Feat user join, get user list

---
+++ src/main/java/com/cool/lib/service/MemberService.java
@@ -0,0 +1,50 @@
+package com.cool.lib.service;
+
+import com.cool.lib.domain.Member;
+import com.cool.lib.repository.MemberRepository;
+import com.cool.lib.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복회원은 X
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m->{
+//            throw new IllegalStateException("이미 존재하는 회원입니다");
+//        });
+        validateDupuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDupuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                       throw  new IllegalStateException("이미 존재하는 회원입니다");
+        });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
